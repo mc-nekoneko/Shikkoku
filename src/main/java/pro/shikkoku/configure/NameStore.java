8d73d8a87914c51a46241fc7d983d7be62bea4a7
Nekoneko <nekonekogame@gmail.com>
2019-01-09 23:31:55 +0900

    プレイヤーのスキンが消えてしまうので削除

--- src/main/java/pro/shikkoku/configure/NameStore.java
+++ src/main/java/pro/shikkoku/configure/NameStore.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import pro.shikkoku.util.JsonConfiguration;
-import pro.shikkoku.util.NameChanger;
 
 import java.io.File;
 import java.util.HashMap;
@@ -17,12 +16,10 @@ import java.util.UUID;
  */
 public class NameStore extends JsonConfiguration {
 
-    private final NameChanger nameChanger;
     private Data data = new Data();
 
     public NameStore(JavaPlugin plugin) {
         super(plugin, new File(plugin.getDataFolder(), "namestore.json"));
-        this.nameChanger = new NameChanger(plugin);
         save(data);
     }
 
@@ -40,14 +37,12 @@ public class NameStore extends JsonConfiguration {
         }
         player.setDisplayName(name);
         player.setPlayerListName(name);
-        nameChanger.changeNameAdd(player, name);
     }
 
     public void reset(Player player) {
         data.names.remove(player.getUniqueId());
         player.setDisplayName(player.getName());
         player.setPlayerListName(player.getName());
-        nameChanger.changeNameRemove(player);
     }
 
     public String getName(Player player) {
